a32c519eb8c36002521dae8de7ba2b8913651db8
Ying Guo <guoguoying2013@gmail.com>
2019-05-20 21:52:35 -0700

    added log in view

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blogging.views import stub_view
-from blogging.views import list_view
+from blogging.views import list_view, detail_view
 
 #first is url path, second is the view function name in the view.py
 urlpatterns = [

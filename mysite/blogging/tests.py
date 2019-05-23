5acc6a423947f78b2507554621f90da7a0378fa4
Ying Guo <guoguoying2013@gmail.com>
2019-05-22 20:52:58 -0700

    corrected test.py and the json file

--- mysite/blogging/tests.py
+++ mysite/blogging/tests.py
@@ -3,8 +3,21 @@ from django.test import TestCase
 from blogging.models import Category, Post
 import datetime
 from django.utils.timezone import utc
+from django.contrib.auth.models import User
 
 # and the test case and test
+class PostTestCase(TestCase):
+   fixtures = ['blogging_test_fixture.json', ]
+
+   def setUp(self):
+       self.user = User.objects.get(pk=1)
+
+   def test_string_representation(self):
+       expected = "This is a title"
+       p1 = Post(title=expected)
+       actual = str(p1)
+       self.assertEqual(expected, actual)
+
 class CategoryTestCase(TestCase):
 
     def test_string_representation(self):
@@ -40,6 +53,7 @@ class FrontEndTestCase(TestCase):
             title = "Post %d Title" % count
             if count < 6:
                 self.assertContains(resp, title, count=1)
+                #remove count=1?
             else:
                 self.assertNotContains(resp, title)
     
